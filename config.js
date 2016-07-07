755275d30b5446c547e9447c1cc70fd3a3a8a94a
Nicole Skoglund <n.c.skoglund@gmail.com>
2016-07-07 00:18:31 -0700

    Add configuration for mock-knex integration tests

--- config.js
+++ config.js
@@ -1,16 +1,33 @@
 // initialize Knex client instance to create a connection pool for the current database
 // see instructions in README.md
-var knex = require('knex')({
-  client: 'mysql',
-  connection: {
-    host     : process.env.TANDEM_DB_HOST || '127.0.0.1',
-    user     : 'tandem',
-    password : process.env.TANDEM_DB_PW || 'oops',
-    database : 'tandem',
-    charset  : 'utf8'
-  },
-  useNullAsDefault: true
-});
+var knex;
+var mockKnex = require('mock-knex');
+
+// mock settings for integration tests
+if (process.env.NODE_ENV === 'test') {
+  knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host: 'FAKE_DATABASE',
+      debug: true
+    }
+  });
+  mockKnex.mock(knex, 'knex@0.10');
+
+// development and production settings
+} else {
+  knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host     : process.env.TANDEM_DB_HOST || '127.0.0.1',
+      user     : 'tandem',
+      password : process.env.TANDEM_DB_PW || 'oops',
+      database : 'tandem',
+      charset  : 'utf8'
+    },
+    useNullAsDefault: true
+  });
+}
 
 // create a bookshelf instance on top of our knex client instance
 // Note: bookshelf can drop down to raw knex interface whenever necessary
